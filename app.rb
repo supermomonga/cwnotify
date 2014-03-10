fd583dee80c0de8d06e4da1dc0bb431d42db06da
supermomonga <hi@supermomonga.com>
2014-03-11 01:02:24 +0900

    push

--- app.rb
+++ app.rb
@@ -14,10 +14,20 @@ class App < Sinatra::Base
 
   post '/github/:room_id' do
     payload = params[:payload] ? JSON.parse(params[:payload]) : nil
-    p params
-    if payload
-      ChatWork::Message.create(room_id: params[:room_id], body: payload.to_s)
+    event_type = request['X-GitHub-Event']
+    return unless payload && event_type
+
+    message = case event_type.to_sym
+    when :push
+      commits = payload[:commits].map{|c|
+        "#{c[:message]}\n  ->#{c[:url]}\n"
+      }.join "\n"
+      "#{payload[:repository][:url]}\n#{payload[:commits].size} commits pushed.\n\n#{commits}"
+    else
+      nil
     end
+
+    ChatWork::Message.create room_id: params[:room_id], body: message if message
   end
 
 end
