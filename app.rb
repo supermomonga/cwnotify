e2dbcd2e0ff8559cb950d403f2643828b3e43334
supermomonga <hi@supermomonga.com>
2014-03-11 00:39:20 +0900

    test for github

--- app.rb
+++ app.rb
@@ -2,9 +2,21 @@
 
 require 'bundler'
 Bundler.require
+require 'json'
+
+ChatWork.api_key = ENV['CHATWORK_API_TOKEN'] || ''
 
 class App < Sinatra::Base
+
   get '/' do
     'Hi there.'
   end
+
+  post '/github/:room_id' do
+    payload = params[:payload] ? JSON.parse(params[:payload]) : nil
+    if payload
+      ChatWork::Message.create(room_id: params[:room_id], body: payload.to_s)
+    end
+  end
+
 end
