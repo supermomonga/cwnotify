e1e579eb12ad0494ae0080dba06f1d8bcacf7b69
supermomonga <hi@supermomonga.com>
2014-04-26 19:28:56 +0900

    bitbucket

--- app.rb
+++ app.rb
@@ -70,6 +70,31 @@ class App < Sinatra::Base
     end
   end
 
+  post '/bitbucket/:room_id' do
+    p params
+    # payload = params[:payload] ? JSON.parse(params[:payload]) : nil
+    # event_type = request.env['HTTP_X_GITHUB_EVENT']
+    # puts "event type: #{event_type}"
+    # return unless payload && event_type
+    #
+    # message = case event_type.to_sym
+    # when :push
+    #   payload['commits'].map{|c|
+    #     title = "[#{c['timestamp']}] #{c['committer']['name']} pushed."
+    #     message = "#{c['message']}\n#{c['url']}"
+    #     CWHelper.tag_info message, title
+    #   }.join "\n"
+    # else
+    #   nil
+    # end
+    #
+    # puts "message: #{message}"
+    # if message
+    #   body = "[CHATWORK NOTIFIER] ヾ(〃l _ l)ﾉﾞ\n#{message}"
+    #   ChatWork::Message.create room_id: params[:room_id], body: body
+    # end
+  end
+
   post '/backuprb/:room_id' do
     status = params[:status]
     icon = case status.to_sym
